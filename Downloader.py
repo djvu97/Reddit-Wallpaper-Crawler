9f06dd5fd66a606c27144b48db6b3a6cc54b0a59
Hiranyey Gajbhiye <hiranyeyg@gmail.com>
2019-01-09 21:08:57 +0530

    Update Downloader.py

--- Downloader.py
+++ Downloader.py
@@ -49,7 +49,7 @@ def WallpaperDownloader(number, Subs, AllWallpaper):
             except:
                 return
             resolution = Image.open("Normal\\"+CURRENT_PIC)
-            if(resolution.size[0]/resolution.size[1] < 1):
+            if(resolution.size[0]/resolution.size[1] < 1 and resolution.size[0]<3000):
                 return False
             ctypes.windll.user32.SystemParametersInfoW(
                 20, 0, os.getcwd()+"\\Normal\\"+timeRightNow+".jpg", 0)
