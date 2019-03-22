7c53578792ab705ec7bd9773881f148c1407069b
Hiranyey Gajbhiye <hiranyeyg@gmail.com>
2019-03-22 13:45:11 +0530

    Update Allsetup.bat

--- Allsetup.bat
+++ Allsetup.bat
@@ -3,7 +3,6 @@ python -V
 If errorlevel 0 (
     pip freeze|findstr praw & if errorlevel 1 (pip install praw)
     pip freeze|findstr wxPython & if errorlevel 1 (pip install -U wxPython)
-    pip freeze|findstr PIL & if errorlevel 1 (pip install Pillow)
     mkdir C:\WallPaper
     move Data\Downloader.bat "C:%homepath%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
     move Data C:\WallPaper\Data
